dc394e296ba2acab1ccb8ee24e3b2146d994d4eb
marvin.baumann <marvin.baumann@kit.edu>
2022-04-08 16:11:45 +0200

    segment_types list for checking function of predecessor and successor

--- german-freeval/input/segmentbuilder.py
+++ german-freeval/input/segmentbuilder.py
@@ -1,33 +1,41 @@
+from attributebuilder import AttributeBuilder
+
+
 class SegmentBuilder:
     def __init__(self, id: int) -> None:
         self.id = id
         self.segments_in = {}
         self.segments_out = {}
-        self.attributes = []
+        self.attribute_builders = []
         pass
 
+    segment_types: list = ["ramp", "base"]
     id: int
     segments_in: dict
     segments_out: dict
-    attributes: list
+    attribute_builders: list
 
-    def add_attribute(self, new_attribute):
-        self.attributes.append(new_attribute)
+    def add_attribute(self, new_attribute_builder: AttributeBuilder):
+        self.attribute_builders.append(new_attribute_builder)
 
     def has_attribute(self, attribute_name):
         pass
 
-    def add_successor(self, successor: "SegmentBuilder", index):
-        if self.segments_out[index]:
+    def add_successor(self, successor: "SegmentBuilder", type: str):
+        if type not in self.segment_types:
+            raise
+        if self.segments_out[type]:
             raise
         else:
-            self.segments_out[index] = successor
+            self.segments_out[type] = successor
 
-    def add_predecessor(self, predecessor: "SegmentBuilder", index):
-        if self.segments_in[index]:
+    def add_predecessor(self, predecessor: "SegmentBuilder", type: str):
+        if type not in self.segment_types:
+            raise
+        if self.segments_in[type]:
             raise
         else:
-            self.segments_in[index] = predecessor
+            self.segments_in[type] = predecessor
 
-    def build():
+    def build(self):
         pass
