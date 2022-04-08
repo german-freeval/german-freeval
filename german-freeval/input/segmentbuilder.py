b61af0fb80f99a5f515c0295ed1caf592ee7d598
Sebastian Buck <38660441+frunika@users.noreply.github.com>
2022-04-08 12:44:45 +0200

    fixed lint error

--- german-freeval/input/segmentbuilder.py
+++ german-freeval/input/segmentbuilder.py
@@ -1,4 +1,3 @@
-
 class SegmentBuilder:
     def __init__(self, id: int) -> None:
         self.id = id
@@ -18,17 +17,17 @@ class SegmentBuilder:
     def has_attribute(self, attribute_name):
         pass
 
-    def add_successor(self, successor: SegmentBuilder, index):
+    def add_successor(self, successor: "SegmentBuilder", index):
         if self.segments_out[index]:
             raise
         else:
             self.segments_out[index] = successor
 
-    def add_predecessor(self, predecessor: SegmentBuilder, index):
+    def add_predecessor(self, predecessor: "SegmentBuilder", index):
         if self.segments_in[index]:
             raise
         else:
             self.segments_in[index] = predecessor
-    
+
     def build():
         pass
