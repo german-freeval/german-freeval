5f1d9a334d58c06267bed890f2df35ebb8744998
Sebastian Buck <38660441+frunika@users.noreply.github.com>
2022-04-09 13:46:14 +0200

    renamed periods

--- tests/context_test.py
+++ tests/context_test.py
@@ -4,11 +4,10 @@ from tests.segment_parsers_test import SegmentParserTest
 
 
 class ContextTest(unittest.TestCase):
-
     def test_create_from(self):
         context = Context.create_from("tests/resources/config.yaml")
 
-        assert context.periods == 10
+        assert context.n_periods == 10
 
         test = SegmentParserTest()
         test.check_topology(context.segment_builders)
