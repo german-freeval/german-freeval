20a9c6d40aee07161da69bec43e90df49190efd6
Jelle Kuebler <91380886+KueblerJelle@users.noreply.github.com>
2022-04-09 17:07:31 +0200

    Added exemplary doc strings

--- german_freeval/meta/app.py
+++ german_freeval/meta/app.py
@@ -3,7 +3,17 @@ from german_freeval.meta.simulator import Simulator
 
 
 class App:
+    """App is the main class of the german_freeval package.
+
+    It initializes the configuration and starts a simulation.
+    """
+
     def main(self, file: str):
+        """Starts a simulationusing the configuration defined in the given file.
+
+        Args:
+            file (str): path to the config file to be used in the simulation
+        """
         context = Context.create_from(file=file)
 
         Simulator(context=context).run()
