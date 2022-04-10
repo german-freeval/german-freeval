384b2d36d22f46a2a2e588cff92edba219205b03
Jelle Kuebler <91380886+KueblerJelle@users.noreply.github.com>
2022-04-10 13:05:37 +0200

    refactored segment dummy changer
    fixes #43

--- german_freeval/macro/dummy_segment_changer.py
+++ german_freeval/macro/dummy_segment_changer.py
@@ -4,59 +4,54 @@ from german_freeval.macro.segment_changer import SegmentChanger
 
 
 class DummySegmentChanger(SegmentChanger):
+
+    VEHICLES_PER_LANE_PER_KM = 100
+    CAPACITY_PER_LANE = 2000
+
     def __init__(self, n_periods: int) -> None:
         super().__init__(n_periods)
 
     def apply(self, segment: Base):
-        capacity = []
-        speed = []
-        density = []
-
         match segment:
             case Source() | Drain():
                 return
             case Merging():
-                ramp_demand = []
+                self.compute_capacity(segment)
+                self.compute_freeflow_speed(segment)
+                self.compute_jam_density(segment)
+                self.compute_demand_ramp(segment)
             case _:
-                ramp_demand = None
+                self.compute_capacity(segment)
+                self.compute_freeflow_speed(segment)
+                self.compute_jam_density(segment)
 
-        for period in range(self.n_periods):
-            capacity.append(self.compute_capacity(segment, period))
-            speed.append(self.compute_freeflow_speed(segment, period))
-            density.append(self.compute_jam_density(segment, period))
-
-            if ramp_demand:
-                ramp_demand.append(self.compute_demand_ramp(segment, period))
-
-        jam_density = PropertyBuilder(
-            name="jam_density", type="float", initial_values=density
-        )
-        capacity_hbs = PropertyBuilder(
+    def compute_capacity(self, segment: Base):
+        capacity = [
+            n_lanes * self.CAPACITY_PER_LANE for n_lanes in segment.lanes.values
+        ]
+        segment.capacity_hbs = PropertyBuilder(
             name="capacity_hbs", type="float", initial_values=capacity
-        )
-        freeflow_speed_hbs = PropertyBuilder(
-            name="freeflow_speed_hbs", type="float", initial_values=speed
-        )
+        ).build(self.n_periods)
 
-        segment.capacity_hbs = capacity_hbs.build(self.n_periods)
-        segment.freeflow_speed_hbs = freeflow_speed_hbs.build(self.n_periods)
-        segment.jam_density = jam_density.build(self.n_periods)
+    def compute_freeflow_speed(self, segment: Base):
+        segment.freeflow_speed_hbs = PropertyBuilder(
+            name="freeflow_speed_hbs",
+            type="float",
+            initial_values=segment.speedlimit.values.copy(),
+        ).build(self.n_periods)
 
-        if ramp_demand:
-            demand_onramp = PropertyBuilder(
-                name="demand_onramp", type="float", initial_values=ramp_demand
-            )
-            segment.demand_onramp = demand_onramp.build(self.n_periods)
+    def compute_jam_density(self, segment: Base):
+        density = [
+            n_lanes * self.VEHICLES_PER_LANE_PER_KM for n_lanes in segment.lanes.values
+        ]
+        segment.jam_density = PropertyBuilder(
+            name="jam_density", type="float", initial_values=density
+        ).build(self.n_periods)
 
-    def compute_capacity(self, segment: Base, period: int):
-        return segment.lanes[period] * 2000
-
-    def compute_freeflow_speed(self, segment: Base, period: int):
-        return segment.speedlimit[period]
-
-    def compute_jam_density(self, segment: Base, period: int):
-        return segment.lanes[period] * 100
-
-    def compute_demand_ramp(self, segment: Merging, period: int):
+    def compute_demand_ramp(self, segment: Merging):
         incoming: Source = segment.ramp_in
-        return incoming.demand[period]
+        segment.demand_onramp = PropertyBuilder(
+            name="demand_onramp",
+            type="float",
+            initial_values=incoming.demand.values.copy(),
+        )
